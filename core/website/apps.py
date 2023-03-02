8db19e10e86383efaa43b7b2fb448a5a27810029
Mohammad Hussein Asnavandi <79707159+mhas1381@users.noreply.github.com>
2023-03-02 22:25:22 +0330

    delete redundant files

--- core/website/apps.py
+++ core/website/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CharityConfig(AppConfig):
+class WebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'charity'
+    name = 'website'
