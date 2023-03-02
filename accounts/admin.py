f4a3e3b2d1d600146ee0e0ffbfa9880fa52a0080
SepehrNorouzi7 <snorouzi07@gmail.com>
2023-03-02 18:28:02 +0330

    Update admin settings

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Profile
 # Register your models here.
 
 class CustomUserAdmin(UserAdmin):
@@ -45,4 +45,5 @@ class CustomUserAdmin(UserAdmin):
         ),
     )
 
+admin.site.register(Profile, CustomUserAdmin)
 admin.site.register(User, CustomUserAdmin)    
